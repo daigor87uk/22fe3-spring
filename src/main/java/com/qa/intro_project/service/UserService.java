9dd5ab247b840e8156433654c20cd5f208ed170a
MrWalshyType2 <mwalsh@qa.com>
2022-04-21 15:08:55 +0100

    Simplified examples used in UserService

--- src/main/java/com/qa/intro_project/service/UserService.java
+++ src/main/java/com/qa/intro_project/service/UserService.java
@@ -1,8 +1,8 @@
 package com.qa.intro_project.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -30,25 +30,42 @@ public class UserService {
 	}
 
 	public List<UserDTO> getUsers() {
-		return userRepository.findAll()
-							 .stream()
-							 .map(this::toDTO)
-							 .collect(Collectors.toList());
+		List<User> users = userRepository.findAll();
+		List<UserDTO> dtos = new ArrayList<>();
+		
+		for (User user : users) {
+			dtos.add(this.toDTO(user));
+		}
+		return dtos;
+//		return userRepository.findAll()
+//							 .stream()
+//							 .map(this::toDTO)
+//							 .collect(Collectors.toList());
 	}
 	
 	public UserDTO getUser(int id) {
 		Optional<User> user = userRepository.findById(id);
-		return this.toDTO(user.orElseThrow(() -> new EntityNotFoundException("User not found with id " + id)));
+		
+		if (user.isPresent()) {
+			return this.toDTO(user.get());
+		}
+		throw new EntityNotFoundException("User not found with id " + id);
+//		return this.toDTO(user.orElseThrow(() -> new EntityNotFoundException("User not found with id " + id)));
 	}
 	
 	// TODO: 5. Implement PostDTO, convert this method to use it
 	public List<Post> getUserPosts(int userId) {
 		Optional<User> user = userRepository.findById(userId);
-		return user.orElseThrow(() -> new EntityNotFoundException("Posts not found with user id " + userId)).getPosts();
+		if (user.isPresent()) {
+			return user.get().getPosts();
+		}
+		throw new EntityNotFoundException("User not found with id " + userId);
+//		return user.orElseThrow(() -> new EntityNotFoundException("Posts not found with user id " + userId)).getPosts();
 	}
 	
 	public UserDTO createUser(NewUserDTO user) {
-		User newUser = userRepository.save(this.modelMapper.map(user, User.class));
+		User toSave = this.modelMapper.map(user, User.class);
+		User newUser = userRepository.save(toSave);
 		return this.toDTO(newUser);
 	}
 	
@@ -61,8 +78,12 @@ public class UserService {
 		// TODO: 2. Implement me
 	}
 	
-	private UserDTO toDTO(User user) {
+	private UserDTO toDTO(User user) {		
 		return this.modelMapper.map(user, UserDTO.class);
+		
+		// ModelMapper will create an instance of UserDTO
+		// - it will then assign the values of all fields in `user`, which have the same name
+		//   as the fields in `UserDTO.class`, to that new instance of UserDTO
 	}
 	
 }
